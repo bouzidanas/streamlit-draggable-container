07e5c78d3be005322045c5092256a80ce7f15f56
bouzidanas <anasbouzid@gmail.com>
2024-06-13 03:34:31 -0500

    first commit

---
+++ experimental.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Get javascript code from file
+with open("streamlit_drag_and_drop.js", "r") as file:
+    js_code = file.read()
+
+
+html = """
+<script>""" + js_code + """    
+</script>
+<div class='elim'></div>
+"""
+components.html(html)
+
+
+def draggable_container(*args, handle=False, destination=False, **kwargs):
+    container = st.container(*args, **kwargs)
+    with container:
+        classes = ['draggable-parent']
+        if not handle:
+            classes.append('no-drag-handle')
+        if not destination:
+            classes.append('no-destination')
+        st.markdown("<div class='draggable-parent elim " + " ".join(classes) + "'></div>", unsafe_allow_html=True)
+
+    return container
+
+st.title("Draggable containers demo")
+st.write("This is a demo testing some javascript and css customizations to make Streamlit containers draggable.")
+
+container = draggable_container(handle=True, border=True)
+
+with container:
+    st.write("This is a draggable container with a handle. Grab the handle to move it around.")
+    title = st.text_input("Movie title", "Life of Brian")
+    st.write("The current movie title is", title)
+    # add a drag handle with a repeating dot pattern
+    st.markdown("<div class='drag-handle' style='height: 15px; width: 100%; display: flex; justify-content: center; align-items: center; border: 1px solid #444444; border-radius: 3px; cursor: pointer;margin-bottom: 0.5rem;margin-top:-0.5rem;'><div style='height: 3px; width: 3px; background-color: #444444; border-radius: 50%; margin: 0 2px;'></div><div style='height: 3px; width: 3px; background-color: #444444; border-radius: 50%; margin: 0 2px;'></div><div style='height: 3px; width: 3px; background-color: #444444; border-radius: 50%; margin: 0 2px;'></div></div>", unsafe_allow_html=True)
+st.write("You can customize the drag handle and drop location indicators. Note that these indicators dissapear when the mouse is not over a set drop destination. If no drop destination is designated, the parent container will be automatically set as the drop destination.")
+
+container2 = st.expander("Buttons")
+with container2:
+    if st.button("Click me"):
+        st.write("Button clicked!")
+
+st.code("print('hello')")
+
+container3 = draggable_container(border=True)
+
+with container3:
+    st.write("This is the second draggable container")
+    st.info("This one has no drag handle. Grab it from anywhere inside the container.")
---
+++ streamlit_drag_and_drop.js
@@ -0,0 +1,165 @@
+root = window.parent.document;
+console.log(root);
+
+let dropZones = [];
+let draggedElement = null;
+let dragIndex = 0;
+
+
+function dragStart(event) {
+    console.log('drag start');
+    console.log(event);
+    draggedElement = event.target;
+}
+
+draggableContainers = root.querySelectorAll('.draggable-parent');
+draggableContainers.forEach(container => {
+    let parent = container.parentNode;
+    while (parent && parent.tagName !== 'BODY') {
+        parent = parent.parentNode;
+        if (parent.dataset.testid === 'stVerticalBlockBorderWrapper') {
+            console.log(parent);
+            if (container.classList.contains('no-drag-handle')) {
+                parent.draggable = true;
+            }
+            else {
+                parent.querySelectorAll('.drag-handle')?.forEach(handle => {
+                    handle.addEventListener('mouseover', function(event) {
+                        parent.draggable = true;
+                    });
+                });
+                parent.querySelectorAll('.drag-handle')?.forEach(handle => {
+                    handle.addEventListener('mouseout', function(event) {
+                        parent.draggable = false;
+                    });
+                });
+            }
+            if (container.classList.contains('no-destination')) {
+                // if dropZones doesnt contain parent.parentNode, add it
+                const dropZone = parent.parentNode;
+                if (!dropZones.includes(dropZone)) {
+                    dropZones.push(dropZone);
+                     // also remove dot if mouse leaves drop container
+                     dropZone?.addEventListener('dragleave', function(event) {
+                        console.log(event);
+                        console.log(dropZone);
+                        let remove = true;
+                        let container = event.fromElement;
+                        while (container && container.tagName !== 'BODY') {
+                            container = container.parentNode;
+                            if (container === dropZone) {
+                                remove = false;
+                            }
+                        }
+                        if (remove && event.fromElement !== dropZone && !event.fromElement.classList.contains('floating-dot')) {
+                            const dots = root.querySelectorAll('.floating-dot');
+                            console.log('remove dots dragleave');
+                            dots.forEach(dot => {
+                                dot.remove();
+                            });
+                        }
+                    });
+                    dropZone?.addEventListener('dragover', function(event) {
+                        event.preventDefault();
+                        console.log(event.target);
+                        
+
+                        const rect = dropZone.getBoundingClientRect();
+                        const y = event.clientY - rect.top;
+                        const children = Array.from(dropZone.children);
+                        let i = 0;
+                        for (const child of children) {
+                            console.log(child.offsetTop + child.offsetHeight / 2, y);
+                            if (child.offsetTop + child.offsetHeight / 2 < y) {
+                                i++;
+                            }
+                        }
+
+                        if (i === dragIndex) {
+                            return;
+                        }
+                        else {
+                            // remove any floating dots
+                            console.log('remove dots' , i, dragIndex);
+                            const dots = root.querySelectorAll('.floating-dot');
+                            dots.forEach(dot => {
+                                dot.remove();
+                            });
+                        }
+                        dragIndex = i;
+
+                        console.log(i);
+                        if (children[i] !== draggedElement && children[i] !== draggedElement.nextSibling) {
+                            const dot = document.createElement('div');
+                            dot.style.position = 'absolute';
+                            // dot.style.width = '4px';
+                            // dot.style.height = '4px';
+                            // dot.style.backgroundColor = '#444444';
+                            // dot.style.borderRadius = '50%';
+                            dot.style.width = '8%';
+                            dot.style.height = '4px';
+                            dot.style.backgroundColor = '#444444';
+                            dot.style.borderRadius = '2px';
+                            dot.style.left = '46%';
+                            if (i === children.length) {
+                                dot.style.top = rect.top + children[i-1].offsetTop + children[i-1].offsetHeight + 10 + 'px';
+                            } else {
+                                dot.style.top = rect.top + children[i].offsetTop - 12 + 'px';
+                            }
+                            dot.classList.add('floating-dot');
+                            dropZone.parentNode.appendChild(dot);
+                        }
+                        
+                    });
+                    dropZone?.addEventListener('drop', function(event) { 
+                        event.preventDefault();
+                       
+                        // remove any floating dots
+                        const dots = root.querySelectorAll('.floating-dot');
+                        dots.forEach(dot => {
+                            dot.remove();
+                        });
+                        
+                        console.log('drop');
+                        console.log(event);
+                        // insert parent at location of drop in dropZone
+                        const rect = dropZone.getBoundingClientRect();
+                        const y = event.clientY - rect.top;
+                        const children = Array.from(dropZone.children);
+                        let i = 0;
+                        for (const child of children) {
+                            console.log(child.offsetTop + child.offsetHeight / 2, y);
+                            if (child.offsetTop + child.offsetHeight / 2 < y) {
+                                i++;
+                            }
+                        }
+                        console.log(i);
+                        dropZone.insertBefore(draggedElement, children[i]);
+                    });
+                }
+            }
+            parent.addEventListener('dragstart', dragStart);
+            break;
+        }
+    }
+});
+
+// hide iframe that contains div with class 'elim' to remove script container from
+// potential drop destination and prevent it from interfering with drag and drop
+const iframe = root.querySelectorAll('iframe');
+iframe.forEach(frame => {
+    const div = frame.contentDocument.querySelector('.elim');
+    if (frame.parentNode.dataset.testid === 'element-container') {
+        frame.parentNode.style.display = 'none';
+    }
+});
+
+// hide element-container containing div with class 'elim' to hide markdown 
+// elements that are added for drag and drop purposes
+const elements = root.querySelectorAll('[data-testid="element-container"]');
+elements.forEach(element => {
+    const div = element.querySelector('.elim');
+    if (div) {
+        element.style.display = 'none';
+    }
+});
